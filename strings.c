6c704e1e46b32983420d50838f6f58eaa43ea518
Chen-Oliver <ochen1@stuy.edu>
2016-09-28 22:30:55 -0400

    added strncat

--- strings.c
+++ strings.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int mystrlen(char s[]){
+int mystrlen(char *s){
   int i=0;
   while(s[i]) i++;
 return i;
@@ -13,7 +13,7 @@ char *mystrcpy(char *dest, char *src){
   *(++dest) = 0;
   return orig;
 }
-char *mystrncpy(char *dest, char *src, size_t len){
+char *mystrncpy(char *dest, char *src, int len){
   int i=0;
   for(;i<len&&src[i]!=0;i++)dest[i] = src[i];
   for(;i<len;i++)dest[i] = 0;
@@ -23,9 +23,14 @@ char *mystrcat(char *dest, char *src){
   mystrcpy(mystrlen(dest)+dest,src);
   return dest;
 }
+char *mystrncat(char *dest, char *src, int len){
+  mystrncpy(mystrlen(dest)+dest,src,len);
+  return dest;
+}
 int main(){
   char a[100] = "cool";
   char b[20] = "cat";
-  printf("%s\n",mystrcat(a,b));
+  printf("%s\n",mystrncpy(a,b,4));
+  printf("%s\n",mystrncat(a,b,2));
   return 0;
 }
